75162f12dee80ec6501ef9b09f0de88066748db4
asarium <asarium@gmail.com>
2016-01-11 13:00:57 +0100

    Print logging output in testsOC

--- test/src/util.cpp
+++ test/src/util.cpp
@@ -24,6 +24,29 @@ namespace
         std::fseek(static_cast<FILE*>(ud), static_cast<long>(offset), whence);
         return static_cast<int64_t>(std::ftell(static_cast<FILE*>(ud)));
     }
+
+    ImgloadErrorCode IMGLOAD_CALLBACK logger(void* ud, ImgloadLogLevel level, const char* text)
+    {
+        switch(level)
+        {
+        case IMGLOAD_LOG_DEBUG:
+            std::cout << "DEBG: ";
+            break;
+        case IMGLOAD_LOG_INFO:
+            std::cout << "INFO: ";
+            break;
+        case IMGLOAD_LOG_WARNING:
+            std::cout << "WARN: ";
+            break;
+        case IMGLOAD_LOG_ERROR:
+            std::cout << " ERR: ";
+            break;
+        }
+
+        std::cout << text;
+
+        return IMGLOAD_ERR_NO_ERROR;
+    }
 }
 
 namespace util
@@ -41,6 +64,8 @@ namespace util
             ctx = nullptr;
             FAIL() << "Failed to allocate imgload context!";
         }
+
+        imgload_context_set_log_callback(ctx, logger, nullptr);
     }
 
     void ContextFixture::TearDown()
