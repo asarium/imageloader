c39670a2abc70bdf6798cc334f6f88112e074df8
asarium <asarium@gmail.com>
2016-01-12 17:52:23 +0100

    Build all targets in Travis CI

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -4,10 +4,10 @@ set -e
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     cd travis-build
-    ninja imgload_test
+    ninja all
     ./test/imgload_test
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    cmake --build travis-build --target imgload_test --config $CONFIGURATION | xcpretty -c
+    cmake --build travis-build --target all --config $CONFIGURATION | xcpretty -c
     ./travis-build/test/$CONFIGURATION/imgload_test
 fi
 
