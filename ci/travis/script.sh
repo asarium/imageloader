f1da05f3fb3b090ddd8254bbc92ad4f37bee47d5
asarium <asarium@gmail.com>
2016-01-15 15:42:52 +0100

    Run Valgrind on Linux Unit tests

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -5,7 +5,8 @@ set -e
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     cd travis-build
     ninja all
-    ./test/imgload_test
+    valgrind --leak-check=full --error-exitcode=1 --gen-suppressions=all \
+        --suppressions="$TRAVIS_BUILD_DIR/ci/travis/valgrind.supp" ./test/imgload_test
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     cmake --build travis-build --config $CONFIGURATION | xcpretty -c
     ./travis-build/test/$CONFIGURATION/imgload_test
