e8ee5887250d1c026e3dee9d08561f23757bc95b
asarium <asarium@gmail.com>
2016-01-15 20:01:07 +0100

    Also fix function signature in implementation

--- src/version.c
+++ src/version.c
@@ -1,16 +1,16 @@
 #include <imageloader.h>
 
-uint32_t IMGLOAD_API imgload_version_major()
+uint32_t IMGLOAD_API imgload_version_major(void)
 {
     return IMGLOAD_VERSION_MAJOR;
 }
 
-uint32_t IMGLOAD_API imgload_version_minor()
+uint32_t IMGLOAD_API imgload_version_minor(void)
 {
     return IMGLOAD_VERSION_MINOR;
 }
 
-uint32_t IMGLOAD_API imgload_version_patch()
+uint32_t IMGLOAD_API imgload_version_patch(void)
 {
     return IMGLOAD_VERSION_PATCH;
 }
