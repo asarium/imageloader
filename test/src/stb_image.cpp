bd812f024535c328cc3c207eb45ad55aee5a96ae
asarium <asarium@gmail.com>
2016-01-16 14:49:23 +0100

    Add TGA test file

--- test/src/stb_image.cpp
+++ test/src/stb_image.cpp
@@ -84,3 +84,77 @@ TEST_F(STBITests, read_data_jpeg_flip)
 
     std::fclose(file_ptr);
 }
+
+TEST_F(STBITests, read_header_tga)
+{
+    ImgloadImage img;
+    auto io = util::get_std_io();
+
+    auto file_ptr = std::fopen(TEST_DATA_PATH "stb_image/FLAG_B24.TGA", "rb");
+
+    ASSERT_EQ(IMGLOAD_ERR_NO_ERROR, imgload_image_init(this->ctx, &img, &io, static_cast<void*>(file_ptr)));
+
+    ASSERT_EQ(1, imgload_image_num_subimages(img));
+    ASSERT_EQ(1, imgload_image_num_mipmaps(img, 0));
+
+    ASSERT_EQ(IMGLOAD_FORMAT_R8G8B8, imgload_image_data_format(img));
+    ASSERT_EQ(IMGLOAD_COMPRESSION_NONE, imgload_image_compression(img));
+
+    uint32_t val;
+    ASSERT_EQ(IMGLOAD_ERR_NO_ERROR, imgload_image_get_property(img, 0, IMGLOAD_PROPERTY_WIDTH, IMGLOAD_PROPERTY_TYPE_UINT32, &val));
+    ASSERT_EQ(124, val);
+
+    ASSERT_EQ(IMGLOAD_ERR_NO_ERROR, imgload_image_get_property(img, 0, IMGLOAD_PROPERTY_HEIGHT, IMGLOAD_PROPERTY_TYPE_UINT32, &val));
+    ASSERT_EQ(124, val);
+
+    ASSERT_EQ(IMGLOAD_ERR_NO_ERROR, imgload_image_get_property(img, 0, IMGLOAD_PROPERTY_DEPTH, IMGLOAD_PROPERTY_TYPE_UINT32, &val));
+    ASSERT_EQ(1, val);
+
+    ASSERT_EQ(IMGLOAD_ERR_NO_ERROR, imgload_image_free(img));
+
+    std::fclose(file_ptr);
+}
+
+TEST_F(STBITests, read_data_tga)
+{
+    ImgloadImage img;
+    auto io = util::get_std_io();
+
+    auto file_ptr = std::fopen(TEST_DATA_PATH "stb_image/FLAG_B24.TGA", "rb");
+
+    ASSERT_EQ(IMGLOAD_ERR_NO_ERROR, imgload_image_init(this->ctx, &img, &io, static_cast<void*>(file_ptr)));
+
+    ASSERT_EQ(IMGLOAD_ERR_NO_ERROR, imgload_image_read_data(img));
+
+    ImgloadImageData data;
+    ASSERT_EQ(IMGLOAD_ERR_NO_DATA, imgload_image_compressed_data(img, 0, 0, &data));
+
+    ASSERT_EQ(IMGLOAD_ERR_NO_ERROR, imgload_image_data(img, 0, 0, &data));
+
+    ASSERT_EQ(IMGLOAD_ERR_NO_ERROR, imgload_image_free(img));
+
+    std::fclose(file_ptr);
+}
+
+TEST_F(STBITests, read_data_tga_flip)
+{
+    this->makeContext(IMGLOAD_CONTEXT_FLIP_IMAGES);
+
+    ImgloadImage img;
+    auto io = util::get_std_io();
+
+    auto file_ptr = std::fopen(TEST_DATA_PATH "stb_image/FLAG_B24.TGA", "rb");
+
+    ASSERT_EQ(IMGLOAD_ERR_NO_ERROR, imgload_image_init(this->ctx, &img, &io, static_cast<void*>(file_ptr)));
+
+    ASSERT_EQ(IMGLOAD_ERR_NO_ERROR, imgload_image_read_data(img));
+
+    ImgloadImageData data;
+    ASSERT_EQ(IMGLOAD_ERR_NO_DATA, imgload_image_compressed_data(img, 0, 0, &data));
+
+    ASSERT_EQ(IMGLOAD_ERR_NO_ERROR, imgload_image_data(img, 0, 0, &data));
+
+    ASSERT_EQ(IMGLOAD_ERR_NO_ERROR, imgload_image_free(img));
+
+    std::fclose(file_ptr);
+}
